67451eb65ee761f80003df8d8c44e6d887eb90ac
joaovictormartin <joao.teixeira@aedb.br>
2021-07-08 17:00:18 -0300

    adicionando a tipagem TS no index

--- src/components/RepoCard/index.tsx
+++ src/components/RepoCard/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-const RepoCard: React.FC = () => {
-  return <div />;
+interface Props {
+  username: string;
+  reponame: string;
+  description?: string;
+  language?: string;
+  start: number;
+  forks: number;
+}
+
+const RepoCard: React.FC<Props> = ({
+  username,
+  reponame,
+  description,
+  language,
+  start,
+  forks,
+}) => {
+  return(
+    <Container>
+      
+    </Container>
+  );
 }
 
 export default RepoCard;
