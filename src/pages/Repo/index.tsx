f25f5d529860a66121cf4987003678509e41ff81
joaovictormartin <joao.teixeira@aedb.br>
2021-07-26 16:05:23 -0300

    criando a tela dos Repositório

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  Breadcrumb, 
+  RepoIcon, 
+  Stats, 
+  StatsIcon, 
+  ForkIcon, 
+  LinkButton, 
+  GithubIcon 
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/joaovictormartin'}>
+          joaovictormartin
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/joaovictormartin/clone-github'}>
+          clone-github
+        </Link>
+      </Breadcrumb>
+
+      <p>Projeto feito em React JS usando TypeScript</p>
+
+      <Stats>
+        <li>
+          <StatsIcon />
+          <b>8</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>4</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/Joaovictormartin/Clone-GitHub'}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
